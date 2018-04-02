acf4ea96b24a561c9bde9689893b79e9670dba3b
LoveinSun <1974301247@qq.com>
2018-04-02 09:15:58 +0800

    .javatijiao

---
+++ pangdehebingdafa.java
@@ -0,0 +1,27 @@
+//copy/b E:\temps\*.ts E:\temps\hebing.ts
+import java.io.*;
+
+public class pangdehebingdafa {
+	public static void main(String []args) {
+		int a = 1;
+		for(a = 1;a<100;a++) {
+			if(a<=9) {
+				String str = String.format("G:\\2\\%d.ts",a);
+				String str2 = String.format("G:\\2\\00%d.ts",a);
+				File file=new File(str); 
+				if(file.exists())
+				{
+				file.renameTo(new File(str2));
+				}
+			}else if(a <= 99) {
+				String str3 = String.format("G:\\2\\%d.ts",a);
+				String str4 = String.format("G:\\2\\0%d.ts",a);
+				File file=new File(str3); 
+				if(file.exists())
+				{
+				file.renameTo(new File(str4));
+				}
+			}
+		}
+	}
+}
